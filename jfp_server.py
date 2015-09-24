3f9e9d217f30c9b547eb0e9968b656d1f9606f4f
David Pickart <pickartd@carleton.edu>
2015-09-24 10:13:12 -0500

    Fix date bug

--- jfp_server.py
+++ jfp_server.py
@@ -63,7 +63,7 @@ def hide_successful():
     cur = g.db.execute('SELECT time, printer, copies, success FROM Entries WHERE success == 0 ORDER BY id DESC')
     entries = [dict(time=format_time(row[0]), printer=row[1], copies=row[2], success=row[3]) for row in cur.fetchall()]
     numreq = g.db.execute('SELECT COUNT(*) FROM Entries').fetchone()[0]
-    dayreq = g.db.execute('SELECT COUNT(*) FROM Entries WHERE time > ?', [time.time()-86400000]).fetchone()[0]
+    dayreq = g.db.execute('SELECT COUNT(*) FROM Entries WHERE time > ?', [time.time()-86400]).fetchone()[0]
     return render_template('layout.html', entries=entries, numrequests=numreq, dayrequests=dayreq)
 
 @app.route('/show', methods=['POST'])
